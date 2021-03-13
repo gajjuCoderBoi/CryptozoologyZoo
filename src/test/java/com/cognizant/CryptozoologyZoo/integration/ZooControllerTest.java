3f03a551c7b1d46fb1c86760756fffa06bff63df
Mohammad Javed <849897@cognizant.com>
2021-03-13 12:51:15 -0500

    Case #1: Red -> Yellow

---
+++ src/test/java/com/cognizant/CryptozoologyZoo/integration/ZooControllerTest.java
@@ -0,0 +1,36 @@
+package com.cognizant.CryptozoologyZoo.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ZooControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void addAnimalTest() throws Exception {
+        RequestBuilder rq = post("/animal")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{}");
+
+        mockMvc
+                .perform(rq)
+                    .andExpect(status().isCreated())
+
+        ;
+
+    }
+
+}
