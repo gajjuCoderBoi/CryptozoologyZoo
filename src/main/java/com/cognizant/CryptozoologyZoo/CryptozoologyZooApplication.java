f278b485b55aa87794f849226a1511869108fecf
Mohammad Javed <849897@cognizant.com>
2021-03-13 12:34:26 -0500

    Init

---
+++ src/main/java/com/cognizant/CryptozoologyZoo/CryptozoologyZooApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.CryptozoologyZoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptozoologyZooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptozoologyZooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cognizant/CryptozoologyZoo/CryptozoologyZooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.CryptozoologyZoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptozoologyZooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
